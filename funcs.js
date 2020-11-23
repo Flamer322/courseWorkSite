a97f1217861546ae609cd5d47c1660bac04aec2d
Flamer322 <56824837+Flamer322@users.noreply.github.com>
2020-11-24 02:29:38 +0300

    upd

---
+++ funcs.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+    $('.header_burger').click(function(event){
+        $('.header_burger, .header_menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+});
+
+$(window).scroll(function() {
+    if($(this).scrollTop() != 0) {
+    $('.toTop').fadeIn();
+    } else {
+    $('.toTop').fadeOut();
+    }
+});
+
+$(document).ready(function(){
+    $('.toTop').click(function(event) {
+        $('body,html').animate({scrollTop:0},700);
+    });
+});
+
+function widget(words){
+    (function (w) {
+        function start() {
+            w.removeEventListener("YaMarketAffiliateLoad", start);
+            for (var i = 0; i < words.length; i++){
+                w.YaMarketAffiliate.createWidget({
+                    containerId: "marketWidget" + (i + 1),
+                    type: "models",
+                    params: {
+                        clid: 2310490,
+                        searchText: words[i],
+                        themeId: 5,
+                        themeBackgroundColor: "rgba(0,0,0,0)"
+                    }
+                });
+            }
+        }
+        w.YaMarketAffiliate
+        ? start()
+        : w.addEventListener("YaMarketAffiliateLoad", start);
+    })(window);
+}
+
